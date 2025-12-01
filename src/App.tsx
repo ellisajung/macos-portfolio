3ac8b4f15b6f3768b844fc4cd5f2fbc305bd3fde
Elisa <ellisajung12@gmail.com>
2025-12-01 15:20:16 +0900

    feat: implement dock ui component (Dock.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Navbar, Welcome } from "#components";
+import { Dock, Navbar, Welcome } from "#components";
 
 const App = () => {
   return (
     <main className="font-">
       <Navbar />
       <Welcome />
+      <Dock />
     </main>
   );
 };
